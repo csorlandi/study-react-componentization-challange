4b14e362183adef9d92db4c18e8ee2c57d60996a
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-17 02:02:42 -0300

    chore: Refactor App component into Content and SideBar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreProps[];
+  handleClickButton: Function;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
